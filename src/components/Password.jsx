549e8ad8141f956af3b4b8dd2204391fb6259508
Dhruv Mehta <flashxx001@gmail.com>
2024-09-01 21:54:17 +0530

    Minor UI Updates.

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,11 +1,30 @@
 import { VscEyeClosed } from "react-icons/vsc";
 import { IoIosEye } from "react-icons/io";
 import { useState } from "react";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 
 const Password = () => {
   const [isOpen, setIsOpen] = useState(false);
+
+  function handleSubmit() {
+    Toastify({
+      text: "Form Submitted Successfully!",
+      duration: 3000,
+      destination: "",
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+      },
+      onClick: function () {}, // Callback after click
+    }).showToast();
+  }
   return (
-    <div className="min-h-[400px] bg-white flex items-center justify-center text-black flex-col max-w-[600px] w-[96%] gap-4">
+    <div className="min-h-[400px] bg-white flex items-center justify-center text-black flex-col max-w-[600px] w-[96%] gap-4 rounded">
       <div className="flex flex-col max-w-full w-[96%]">
         <label className="max-w-[300px] w-[99%]">Enter Name Here</label>
         <input
@@ -33,7 +52,10 @@ const Password = () => {
           />
         )}
       </div>
-      <button className="px-10 py-3 text-white font-semibold text-md bg-green-600">
+      <button
+        onClick={handleSubmit}
+        className="px-10 py-3 text-white font-semibold text-md bg-green-600 rounded hover:bg-green-400"
+      >
         Submit
       </button>
     </div>
